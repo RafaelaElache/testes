d3029867099c26eb96c29510fa3d38f9340c0eb6
LivVanessa <105389707+LivVanessa@users.noreply.github.com>
2024-10-28 21:16:26 -0300

    Create teste2_step.rb
    Criação de um código inicial do teste 2 em ruby

---
+++ testes/features/step_definitions/teste2_step.rb
@@ -0,0 +1,11 @@
+require 'capybara/cucumber'
+require 'selenium-webdriver'
+require 'capybara/rspec'
+
+Given('I am on the homepage') do
+  click on 
+end
+
+When('I 
+
+Then('I should be redirected to http://devcolab.each.usp.br/dadosorcamentarios')
