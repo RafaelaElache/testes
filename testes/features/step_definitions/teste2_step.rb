6847cc4a7a97863e212782236b273c8cfe15b21b
LivVanessa <105389707+LivVanessa@users.noreply.github.com>
2024-10-28 21:37:01 -0300

    Update teste2_step.rb

--- testes/features/step_definitions/teste2_step.rb
+++ testes/features/step_definitions/teste2_step.rb
@@ -3,9 +3,7 @@ require 'selenium-webdriver'
 require 'capybara/rspec'
 
 Given('I am on the homepage') do
-  click on 
+  click on 'BAIXAR TABELA' button
 end
-
-When('I 
 
 Then('I should be redirected to http://devcolab.each.usp.br/dadosorcamentarios')
