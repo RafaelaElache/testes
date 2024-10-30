e0072287fab9d78430557f1731e942b338485bde
LivVanessa <105389707+LivVanessa@users.noreply.github.com>
2024-10-30 16:13:09 -0300

    Update teste2_step.rb

--- testes/features/step_definitions/teste2_step.rb
+++ testes/features/step_definitions/teste2_step.rb
@@ -3,7 +3,13 @@ require 'selenium-webdriver'
 require 'capybara/rspec'
 
 Given('I am on the homepage') do
-  click on 'BAIXAR TABELA' button
+  visit('/')
 end
 
-Then('I should be redirected to http://devcolab.each.usp.br/dadosorcamentarios')
+When('I click on the {string} button') do |button_text|
+  click_button(button_text)
+end
+
+Then('I should be redirected to {string}') do |expected_url|
+  expect(page.current_url).to eq(expected_url)
+end
