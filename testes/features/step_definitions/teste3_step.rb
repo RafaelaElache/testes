5b880784e175e70a48d2836cad2a1841e4eb3d44
Luiza Silva e Rego <117910108+Luizarego@users.noreply.github.com>
2024-10-30 22:03:11 -0300

    Update concerta teste3_step.rb

--- testes/features/step_definitions/teste3_step.rb
+++ testes/features/step_definitions/teste3_step.rb
@@ -4,10 +4,11 @@ Dado('que já estou na página inicial do site "Cuidando do Meu Bairro"') do
 end
 
 Quando('clico no botão "Blog"') do
-  expect(page).to have_selector('#navbar > ul > li:nth-child(4) > a')  
+  expect(page).to have_selector('#navbar > ul > li:nth-child(4) > a') 
+  find('#navbar > ul > li:nth-child(4) > a').click
 end
 
-Então('devo ser redirecionado para a página do blog em {string}') do |expected_url|
+Então('devo ser redirecionado para a página do blog') do
   switch_to_window { title == 'Cuidando do Meu Bairro | Co:Lab' }
-  assert_current_path(expected_url)
+  assert_current_path('https://colab.each.usp.br/blog/tag/cuidando-do-meu-bairro/')
 end
