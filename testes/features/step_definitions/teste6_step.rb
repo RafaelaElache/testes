6e1560dad739fbaa7338ebf2ef55513ec5496c31
Luiza Silva e Rego <117910108+Luizarego@users.noreply.github.com>
2024-10-30 21:18:28 -0300

    Update teste6_step.rb

--- testes/features/step_definitions/teste6_step.rb
+++ testes/features/step_definitions/teste6_step.rb
@@ -6,6 +6,6 @@ When('clico no botão "Aprenda+"') do
   find_button('Aprenda +').click 
 end
 
-Then('devo ser redirecionado para a página de glossário em {string}') do |https://cuidando.vc/glossario|
-  expect(page).to have_current_path(https://cuidando.vc/glossario, url: true)
+Then('devo ser redirecionado para a página de glossário em {string}') do |expected_url|
+  expect(page).to have_current_path(expected_url, url: true)
 end
