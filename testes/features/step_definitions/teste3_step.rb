baa63bedb5d57924b937fee5befbb0d1de46113d
Luiza Silva e Rego <117910108+Luizarego@users.noreply.github.com>
2024-10-30 21:39:41 -0300

    Update teste3_step.rb

--- testes/features/step_definitions/teste3_step.rb
+++ testes/features/step_definitions/teste3_step.rb
@@ -1,11 +1,12 @@
-Given('que já estou na página inicial do site "Cuidando do Meu Bairro"') do
+#language: pt
+Dado('que já estou na página inicial do site "Cuidando do Meu Bairro"') do
   visit 'https://cuidando.vc/2022/1'
 end
 
-When('clico no botão "Blog"') do
-  find_button('blog.cuidando.vc').click  
+Quando('clico no botão "Blog"') do
+  expect(page).to have_selector(<a data-v-d889a2de="" href="http://blog.cuidando.vc" target="_blank" class="p-4 mx-1 hover:py-2 hover:bg-primary-dark hover:bg-opacity-80 hover:rounded">Blog</a>)  
 end
 
-Then('devo ser redirecionado para a página do blog em {string}') do |expected_url|
+Então('devo ser redirecionado para a página do blog em {string}') do |expected_url|
   expect(page).to have_current_path(expected_url, url: true)
 end
